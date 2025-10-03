0cbdbb5f781f038ebf507142d2df1f07bd5471f5
yassyass2 <yassinabde@outlook.com>
2025-10-03 11:42:00 +0200

    matching algorithm 335 kwh per jaar

--- Avisi2025/backend/app/main.py
+++ Avisi2025/backend/app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.api import verbruik
+from app.api.matching import router as matching_router 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 # Include routers
 app.include_router(verbruik.router, prefix="/api/v1", tags=["verbruik"])
+app.include_router(matching_router, prefix="/api/consumer", tags=["matching"])
 
 origins = [
     "*",
